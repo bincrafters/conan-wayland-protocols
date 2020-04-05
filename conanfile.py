034e0d3d3698db8f5c84916e4085d8b3f6836893
Eric Lemanissier <eric.lemanissier@gmail.com>
2020-04-05 16:54:10 +0200

    wayland-protocols 1.20

--- conanfile.py
+++ conanfile.py
@@ -1,62 +1,52 @@
-from conans import ConanFile, CMake, tools
-import os
+from conans import ConanFile, AutoToolsBuildEnvironment, tools
+import os, shutil
 
 
-class LibnameConan(ConanFile):
-    name = "libname"
-    description = "Keep it short"
-    topics = ("conan", "libname", "logging")
-    url = "https://github.com/bincrafters/conan-libname"
-    homepage = "https://github.com/original_author/original_lib"
-    license = "MIT"  # Indicates license type of the packaged library; please use SPDX Identifiers https://spdx.org/licenses/
-    exports_sources = ["CMakeLists.txt"]
-    generators = "cmake"
+class WaylandProtocolsConan(ConanFile):
+    name = "wayland-protocols"
+    description = "The Wayland-Protocols package contains additional Wayland protocols that add functionality outside of protocols already in the Wayland core."
+    topics = ("conan", "wayland-protocols", "wayland")
+    url = "https://github.com/bincrafters/conan-wayland-protocols"
+    homepage = "https://wayland.freedesktop.org/"
+    license = "MIT"
+    generators = "pkg_config"
 
-    settings = "os", "arch", "compiler", "build_type"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = {"shared": False, "fPIC": True}
+    settings = []
+    options = {}
+    default_options = {}
 
     _source_subfolder = "source_subfolder"
-    _build_subfolder = "build_subfolder"
-    _cmake = None
+    _autotools = None
 
     requires = (
-        "zlib/1.2.11"
+        "wayland/1.18.0@bincrafters/stable"
     )
-
-    def config_options(self):
-        if self.settings.os == 'Windows':
-            del self.options.fPIC
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
         extracted_dir = self.name + "-" + self.version
         os.rename(extracted_dir, self._source_subfolder)
 
-    def _configure_cmake(self):
-        if not self._cmake:
-            self._cmake = CMake(self)
-            self._cmake.definitions["BUILD_TESTS"] = False  # example
-            self._cmake.configure(build_folder=self._build_subfolder)
-        return self._cmake
+    def _configure_autotools(self):
+        if not self._autotools:
+            self._autotools = AutoToolsBuildEnvironment(self)
+            self._autotools.configure(configure_dir= self._source_subfolder)
+        return self._autotools
 
     def build(self):
-        cmake = self._configure_cmake()
-        cmake.build()
+        lib_path = self.deps_cpp_info['wayland'].rootpath
+        for dirpath, _, filenames in os.walk(lib_path):
+            for filename in filenames:
+                if filename.endswith('.pc'):
+                    shutil.copyfile(os.path.join(dirpath, filename), filename)
+                    tools.replace_prefix_in_pc_file(filename, lib_path)
+        autotools = self._configure_autotools()
+        autotools.make()
 
     def package(self):
-        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
-        cmake = self._configure_cmake()
-        cmake.install()
-        # If the CMakeLists.txt has a proper install method, the steps below may be redundant
-        # If so, you can just remove the lines below
-        include_folder = os.path.join(self._source_subfolder, "include")
-        self.copy(pattern="*", dst="include", src=include_folder)
-        self.copy(pattern="*.dll", dst="bin", keep_path=False)
-        self.copy(pattern="*.lib", dst="lib", keep_path=False)
-        self.copy(pattern="*.a", dst="lib", keep_path=False)
-        self.copy(pattern="*.so*", dst="lib", keep_path=False)
-        self.copy(pattern="*.dylib", dst="lib", keep_path=False)
+        self.copy(pattern="COPYING", dst="licenses", src=self._source_subfolder)
+        autotools = self._configure_autotools()
+        autotools.install()
 
     def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
+        pass
--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,17 +1,8 @@
-from conans import ConanFile, CMake, tools
+from conans import ConanFile
 import os
 
 
 class TestPackageConan(ConanFile):
-    settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake"
-
-    def build(self):
-        cmake = CMake(self)
-        cmake.configure()
-        cmake.build()
 
     def test(self):
-        if not tools.cross_building(self.settings):
-            bin_path = os.path.join("bin", "test_package")
-            self.run(bin_path, run_environment=True)
+        self.run('wayland-scanner client-header --strict %s /dev/null' % os.path.join(self.deps_cpp_info['wayland-protocols'].rootpath, "share", "wayland-protocols", "stable", "viewporter", "viewporter.xml"), run_environment=True)
